f6ab6b073ccf6687ed1b371479fa7f39d555dce3
jmo <jmo@kabal.(none)>
2010-04-02 09:15:45 -0500

    little more secure ;-)

---
+++ config/app.php
@@ -0,0 +1,29 @@
+<?php
+
+$_app = array (
+  'db' => array(
+    'local' => array(
+      'host' => 'localhost',
+      'user' => 'root',
+      'pass' => 'beans',
+      'name' => 'ff'
+    ),
+  ), 
+
+  'server' => array(
+    'drop' => array(
+      'host' => 'localhost',
+      'user' => 'jmo',
+      'dir' => '/home/jmo/Dropbox/code/flava_framework',
+    ),
+  ),
+
+  'text' => array(
+    'title' => 'base title - ', 
+  ),
+
+  'feature' => array(
+    'module_js' => 1,
+    'module_css' => 1,
+  ),
+);
